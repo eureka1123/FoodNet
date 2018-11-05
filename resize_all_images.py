c91301ed5a386af96fb3c8db247f577b591cf3b9
Daniel Sun <dxsun1998@gmail.com>
2018-11-05 17:01:09 -0500

    commented resize file lol

--- resize_all_images.py
+++ resize_all_images.py
@@ -2,14 +2,31 @@ import PIL
 from PIL import Image
 import os
 
+"""
+Note: You need the Yummly28K directory (the data) to resize the images
+
+Resizes all images inside Yummly dataset to the specified size, 
+currently set to (250, 167)
+
+Creates two directors, resized_alias/ and resized_antialias/ which 
+contain the images resized to the target size, but one uses the antialiasing
+resampling filter
+"""
+
+# Specified target size to resize to
 target_size = (250, 167)
+
+# Filepath to data directory
 base_filepath = "Yummly28K/images27638/"
 
+# Names of files to output resized images
 alias_dir = "resized_alias"
 antialias_dir = "resized_antialias"
 
-os.mkdir(alias_dir)
-os.mkdir(antialias_dir)
+if alias_dir not in os.listdir():
+	os.mkdir(alias_dir)
+if antialias_dir not in os.listdir():
+	os.mkdir(antialias_dir)
 
 total_files = len(['_' for file in os.listdir(base_filepath) if 'DS' not in file])
 
@@ -27,4 +44,4 @@ for file in os.listdir(base_filepath):
 			img_antialias.save(antialias_dir + "/" + file)
 			
 			img_alias = img.resize(target_size)
-			img_alias.save(alias_dir + "/" + file)
+			img_alias.save(alias_dir + "/" + file)	
