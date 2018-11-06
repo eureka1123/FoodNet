46c5dcff7feb3160c1cb731371195a2b0303d00e
xiaolu20 <xiaolu20@mit.edu>
2018-11-05 23:09:46 -0500

    a mess

--- initial_model.py
+++ initial_model.py
@@ -15,6 +15,76 @@ from PIL import Image
 BASE_DIR = os.path.dirname(__file__)
 images_dir = BASE_DIR + "resized_alias/"
 labels_dir = BASE_DIR + "ingredient_vector/"
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.conv3 = nn.Conv2d(20, 30, kernel_size=5)
+        self.fc1 = nn.Linear(118*380, 32)
+        # self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 10)
+
+    def forward(self, x):
+        print("x",x.size())
+        print("conv1",self.conv1(x).size())
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        print("after",x.size())
+        # x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 118*380)
+        x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        return F.log_softmax(x, dim=1)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    sum_num_correct = 0
+    sum_loss = 0
+    num_batches_since_log = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.MultiLabelMarginLoss(output,target)
+        # loss = F.nll_loss(output, target)
+        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct = pred.eq(target.view_as(pred)).sum().item()
+        sum_num_correct += correct
+        sum_loss += loss.item()
+        num_batches_since_log += 1
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{:05d}/{} ({:02.0f}%)]\tLoss: {:.6f}\tAccuracy: {:02.0f}%'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), sum_loss / num_batches_since_log, 
+                100. * sum_num_correct / (num_batches_since_log * train_loader.batch_size))
+            )
+            sum_num_correct = 0
+            sum_loss = 0
+            num_batches_since_log = 0
+
+def test(model, device, test_loader, dataset_name="Test set"):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += nn.MultiLabelMarginLoss(output, target, reduction='sum').item() # sum up batch loss
+            # test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\n{}: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        dataset_name,
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
 def load_datasets(train_percent = .8):
     usable_filenames = sorted(set([i[3:-4] for i in os.listdir(images_dir)]).intersection(set([i[:-4] for i in os.listdir(labels_dir)])))
     images_tensor = []
@@ -40,5 +110,43 @@ def load_datasets(train_percent = .8):
     train_dataset, test_dataset = data_utils.random_split(full_dataset, [train_size, len(full_dataset)-train_size])
 
     return train_dataset, test_dataset
-    
-load_datasets()
+
+def training_procedure(train_dataset, test_dataset):
+    args = dict()
+    args["seed"] = 73912
+    args["no_cuda"] = False
+    args["log_interval"] = 100
+    args["batch_size"] = 2
+    args["test-batch-size"] = 1000
+
+    params = dict()
+    params["epochs"] = 10
+    params["lr"] = 0.1
+
+    torch.manual_seed(args["seed"])
+    use_cuda = not args["no_cuda"] and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                        batch_size=args["batch_size"], shuffle=True, **kwargs)
+
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                        batch_size=args["test-batch-size"], shuffle=True, **kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=params["lr"])
+
+    # Train the model
+    for epoch in range(1, params["epochs"] + 1):
+        train(model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+
+if __name__ == '__main__':
+    train_dataset, test_dataset= load_datasets()
+    training_procedure(train_dataset, test_dataset)
+
+
+
+
